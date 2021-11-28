950a07c5de393d35ccf067678d78c2e3aed1ef27
Gabriel <gabriel.joe@tptlive.ee>
2021-11-28 22:18:31 +0200

    Created map system

---
+++ maps.js
@@ -0,0 +1,12 @@
+let Maps = {
+    map1: {
+        objects: [
+            new Object(0, canvas.height - 10, canvas.width, 20, '#666'),
+            new Object(900, 760, 100, 200, '#666'),
+            new Object(700, 760, 10, 100, '#666'),
+            new MovableObject(500, 500, 50, 50, '#666'),
+            new Coin (600, 800, 25, 25, '#ECFF00'),
+            new Spike(20, 700, 20, 100, '#FF5858')
+        ]
+    }
+};
