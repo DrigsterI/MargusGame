b8a7d897bf237296c5ddd2d05ed9bf978ceadfe5
LesDen <77448042+LesDen@users.noreply.github.com>
2021-11-28 13:45:59 +0300

    Added a button "start game" and a button to return to the menu.

---
+++ Menu.js
@@ -0,0 +1,16 @@
+function StartGame() {
+  document.getElementById('StartButton').style.visibility = 'hidden';
+  document.getElementById('SecondButton').style.visibility = 'hidden';
+  document.getElementById('ThirdButton').style.visibility = 'hidden';
+  document.getElementById('game').style.visibility = 'visible';
+  document.getElementById('game').style.width = "100%";
+  document.getElementById('BackToMenuButton').style.visibility = 'visible';
+}
+function BackToMenu() {
+  document.getElementById('StartButton').style.visibility = 'visible';
+  document.getElementById('SecondButton').style.visibility = 'visible';
+  document.getElementById('ThirdButton').style.visibility = 'visible';
+  document.getElementById('game').style.visibility = 'hidden';
+  document.getElementById('game').style.width = "60%";
+  document.getElementById('BackToMenuButton').style.visibility = 'hidden';
+}
