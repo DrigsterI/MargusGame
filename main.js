e77a0d60723ec4eade50f928199e503963b135a4
ZAK <markur567@gmail.com>
2021-11-28 14:49:57 +0200

    CheckPart() done
    trouble - dont work with small objects

--- main.js
+++ main.js
@@ -91,38 +91,22 @@ class Player {
 			let obstacle = obstacles[i];
 			
 			//левый верхний угол
-			if (this.x >= obstacle.x && this.x <= obstacle.x + obstacle.width &&
-				this.y >= obstacle.y && this.y <= obstacle.y + obstacle.height)
-			{
-				//this.x = 200;
-				//this.y = 200;
+			if (this.x >= obstacle.x && this.x <= obstacle.x + obstacle.width && this.y >= obstacle.y && this.y <= obstacle.y + obstacle.height){
 				this.CheckPart(obstacle);
 			}
 			
 			//правый верхний угол
-			if (this.x + this.width >= obstacle.x && this.x + this.width <= obstacle.x + obstacle.width &&
-				this.y >= obstacle.y && this.y <= obstacle.y + obstacle.height)
-			{
-				//this.x = 200;
-				//this.y = 200;
+			if (this.x + this.width >= obstacle.x && this.x + this.width <= obstacle.x + obstacle.width && this.y >= obstacle.y && this.y <= obstacle.y + obstacle.height){
 				this.CheckPart(obstacle);
 			}
 			
 			//левый нижний угол 
-			if (this.x >= obstacle.x && this.x <= obstacle.x + obstacle.width &&
-				this.y + this.height >= obstacle.y && this.y + this.height <= obstacle.y + obstacle.height)
-			{
-				//this.x = 1200;
-				//this.y = 200;
+			if (this.x >= obstacle.x && this.x <= obstacle.x + obstacle.width && this.y + this.height >= obstacle.y && this.y + this.height <= obstacle.y + obstacle.height){
 				this.CheckPart(obstacle);
 			}
 			
 			//правый нижний угол
-			if (this.x + this.width >= obstacle.x && this.x + this.width <= obstacle.x + obstacle.width &&
-				this.y + this.height >= obstacle.y && this.y + this.height <= obstacle.y + obstacle.height)
-			{
-				//this.x = 200;
-				//this.y = 200;
+			if (this.x + this.width >= obstacle.x && this.x + this.width <= obstacle.x + obstacle.width && this.y + this.height >= obstacle.y && this.y + this.height <= obstacle.y + obstacle.height){
 				this.CheckPart(obstacle);
 			}
 		}
@@ -134,98 +118,76 @@ class Player {
 		let x;
 		let y;
 		
-		// 1 четверть
-		if (this.x >= obstacle.width / 2 && this.y + this.height <= obstacle.height / 2)
-		{
-			x = (obstacle.x + obstacle.width) - this.x;
-			y = this.y - obstacle.y;			
-			if (x > y)
-			{
-				//this.x += x;
-				//this.x = obstacle.x + obstacle.width;
+		
+		// 1 четверть // ВРОДЕ РАБОТАЕТ
+		if (this.x >= ((obstacle.x) + obstacle.width / 2) && this.y + this.height <= ((obstacle.y) + obstacle.height / 2) ||
+			this.x >= ((obstacle.x) + obstacle.width / 2) && this.y <= ((obstacle.y) + obstacle.height / 2) ||
+			this.x + this.width >= ((obstacle.x) + obstacle.width / 2) && this.y + this.height <= ((obstacle.y) + obstacle.height / 2)){
+				x = -(((obstacle.x) + obstacle.width / 2) - this.x);
+				y = ((obstacle.y) + obstacle.height / 2) - (this.y + this.height);	
+			
+				if (x > y){
+					this.x = obstacle.x + obstacle.width+1;
+				}
+				else {
+					this.y = obstacle.y - this.height;
+					this.VelocityX = 0;
+					this.VelocityY = 0;
+				}
 			}
-			else 
-			{
-				//this.y += y;
-				//this.y = obstacle.y - this.height;
-				this.y = obstacle.y + this.height;
-			}
-			console.log("1");
-		}
-
+		
+		
 		// 2 четверть
-		if (this.x + this.width <= ((obstacle.x) + obstacle.width / 2) && this.y + this.height <= ((obstacle.y) + obstacle.height / 2))
-		{
-			x = this.x - obstacle.x;
-			y = this.y - obstacle.y;
-			if (x > y)
-			{
-				//this.x -= x;
-				this.x = obstacle.x - this.width;
-			}
-			else 
-			{
-				//this.y += y;
-				this.y = obstacle.y - this.height-100;
-				//this.y = obstacle.y + this.height;
-			}
-			console.log("2");
-		}
-
-
-		// 3 четверть
-		if (this.x + this.width <= obstacle.width / 2 && this.y >= ((obstacle.y) + obstacle.height / 2) || 
-			this.x <= ((obstacle.x) + obstacle.width / 2) && this.y <= ((obstacle.y) + obstacle.height / 2))
-		{
-			x = this.x - obstacle.x;
-			y = (obstacle.y + obstacle.height) - this.y;
-			if (x > y)
-			{
-				//this.x += x;
-				this.x = obstacle.x - this.width;
-			}
-			else 
-			{
-				//this.y -= y;
-				this.y = obstacle.y - this.height;
+		if (this.x + this.width <= ((obstacle.x) + obstacle.width / 2) && this.y + this.height <= ((obstacle.y) + obstacle.height / 2) ||
+			this.x + this.width <= ((obstacle.x) + obstacle.width / 2) && this.y <= ((obstacle.y) + obstacle.height / 2) ||
+			this.x <= ((obstacle.x) + obstacle.width / 2) && this.y + this.height <= ((obstacle.y) + obstacle.height / 2)){
+				x = this.x - obstacle.x;
+				y = this.y - obstacle.y;
 				
+				if (x < y){
+					this.x = obstacle.x - this.width-1;
+				}
+				else {
+					this.y = obstacle.y - this.height;
+				}
 			}
-				//console.log(this.x);
-				//console.log(this.y);
-				//console.log(" ");
-				//console.log("ok");
-				console.log("3");
-		}
+		
 
+		
+		// 3 четверть
+		if (this.x + this.width <= ((obstacle.x) + obstacle.width / 2) && this.y >= ((obstacle.y) + obstacle.height / 2) ||
+			this.x <= ((obstacle.x) + obstacle.width / 2) && this.y >= ((obstacle.y) + obstacle.height / 2) ||
+			this.x + this.width <= ((obstacle.x) + obstacle.width / 2) && this.y + this.height >= ((obstacle.y) + obstacle.height / 2)){
+				x = this.x - obstacle.x;
+				y = (obstacle.y + obstacle.height) - (this.y + this.height);
+				
+				if (x < y){
+					this.x = obstacle.x - this.width;
+				}
+				else {
+					this.y = (obstacle.y + obstacle.height);
+				}
+			}
+		
 
-
+		
 		// 4 четверть
-		if (this.x >= ((obstacle.x) + obstacle.width / 2) && this.y >= ((obstacle.y) + obstacle.height / 2))
-		{
-			x = (obstacle.x + obstacle.width) - this.x;
-			y = (obstacle.y + obstacle.height) - this.y;
-			if (x < y)
-			{
-				//console.log("ok");
-				//this.x -= x;
-				this.x = obstacle.x + obstacle.width;
+		if (this.x >= ((obstacle.x) + obstacle.width / 2) && this.y >= ((obstacle.y) + obstacle.height / 2) ||
+			this.x + this.width >= ((obstacle.x) + obstacle.width / 2) && this.y >= ((obstacle.y) + obstacle.height / 2) ||
+			this.x >= ((obstacle.x) + obstacle.width / 2) && this.y + this.height >= ((obstacle.y) + obstacle.height / 2)){
+				x = (obstacle.x + obstacle.width) - this.x;
+				y = (obstacle.y + obstacle.height) - this.y;
+				
+				if (x < y) {
+					this.x = obstacle.x + obstacle.width;
+				}
+				else {
+					this.y = obstacle.y + this.height;
+				}
+				//this.VelocityX = 0; //фича
+				//this.VelocityY = 0;
 			}
-			else 
-			{
-				//this.y -= y;
-				this.y = obstacle.y + this.height;
-			}
-			console.log("4");
 		}
-		
-		//console.log(x);
-		//console.log(y);
-		//console.log(" ");
-		console.log(this.x);
-		console.log(this.y);
-		console.log(" ");
-		
-	}
 	
 	
 	
@@ -296,8 +258,10 @@ function Start () {
 
 	player = new Player(400, 800, 50, 50, '#FF5858');
 
-	box = new Obstacle(500, 800, 50, 50, '#666');
+	box = new Obstacle(700, 800, 20, 50, '#666');
+	box1 = new Obstacle(500, 800, 50, 50, '#666');
 	obstacles.push(box);
+	obstacles.push(box1);
 
 	VelocityText = new Text("Velocity: " + 0, 25, 25, "left", "#212121", "20");
 	PosText = new Text("Pos: " + 0, 25, 50, "left", "#212121", "20");
