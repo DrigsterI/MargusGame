2c3ffecfc3049602f78ae4003786fe7a2ebc3015
LesDen <77448042+LesDen@users.noreply.github.com>
2021-11-30 01:43:08 +0300

    Canvas added

--- Menu.js
+++ Menu.js
@@ -1,19 +1,14 @@
 function StartGame() {
-  document.getElementById('StartButton').style.visibility = 'hidden';
-  document.getElementById('SecondButton').style.visibility = 'hidden';
-  document.getElementById('ThirdButton').style.visibility = 'hidden';
-  document.getElementById('game').style.visibility = 'visible';
-  document.getElementById('game').style.width = "100%";
-  document.getElementById('BackToMenuButton').style.visibility = 'visible';
-  document.getElementById('NoSize').style.width = "10%";
+  document.getElementById('firstsection').style.display = "none";
+  document.getElementById('secondsection').style.display = "block";
+  document.body.style.backgroundColor = "white";
+  document.getElementById("secondsection").style.border = "4px solid red";
   Start();
 }
 function BackToMenu() {
-  document.getElementById('StartButton').style.visibility = 'visible';
-  document.getElementById('SecondButton').style.visibility = 'visible';
-  document.getElementById('ThirdButton').style.visibility = 'visible';
-  document.getElementById('game').style.visibility = 'hidden';
-  document.getElementById('game').style.width = "60%";
-  document.getElementById('BackToMenuButton').style.visibility = 'hidden';
+  document.getElementById('firstsection').style.display = "block";
+  document.getElementById('secondsection').style.display = "none";
+  document.body.style.backgroundColor = "white";
+  document.getElementById("secondsection").style.border = "none";
   Stop();
 }
