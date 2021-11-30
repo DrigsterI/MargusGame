7691eeb369e32a71956df3a0bf7593d878b5f137
HckwidLFane <denissslesnikov@gmail.com>
2021-11-30 21:10:57 +0300

    Settings button clickable (but no functionality)

--- Menu.js
+++ Menu.js
@@ -3,6 +3,8 @@ function StartGame() {
   document.getElementById('secondsection').style.display = "block";
   document.body.style.backgroundColor = "white";
   document.getElementById("secondsection").style.border = "4px solid red";
+  document.getElementById('thirdsection').style.display = "none";
+  document.getElementById('invisiblebutton').style.display = "none";
   Start();
 }
 function BackToMenu() {
@@ -11,4 +13,12 @@ function BackToMenu() {
   document.body.style.backgroundColor = "white";
   document.getElementById("secondsection").style.border = "none";
   Stop();
+}
+function settings(){
+  document.getElementById('thirdsection').style.display = "block";
+  document.getElementById('invisiblebutton').style.display = "block";
+}
+function settingsof(){
+  document.getElementById('thirdsection').style.display = "none";
+  document.getElementById('invisiblebutton').style.display = "none";
 }
